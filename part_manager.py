37a20bd104be422e3ae7062959b68bdbaa90ab91
Braydon Millard <braydonmillard@users.noreply.github.com>
2022-04-10 14:20:39 -0400

    first commit

---
+++ part_manager.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+#Create window object
+app = Tk()
+
+# Part
+part_text = StringVar()
+part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
+part_label.grid(row=0, column=0)
+
+
+
+app.title('Part Manager')
+app.geometry('700x350')
+
+
+#Start program
+app.mainloop()
